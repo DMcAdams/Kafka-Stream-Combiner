91562c72ddd637ff53fbf5581af5d992234140ed
Darrin McAdams <darrin.a.mcadams@gmail.com>
2023-07-20 19:27:45 -0500

    Update to Kafka Demo
    Added extra dependencies and maven now builds object classes based on avro schemas in src/main/avro

--- kafkaDemo/src/main/java/com/example/kafkaDemo/KafkaDemoApplication.java
+++ kafkaDemo/src/main/java/com/example/kafkaDemo/KafkaDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.kafkaDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
+@EnableKafkaStreams
 public class KafkaDemoApplication {
 
 	public static void main(String[] args) {
